092900f209136272d0b0b8facd6e20dc49daeefb
Alex <Alexsamarkinru@gmail.com>
2020-02-17 16:05:16 +0300

    Добавьте файлы проекта.

---
+++ Hello/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // cw
+            // Console.WriteLine("Меня зовут Александр");
+
+            // string MyName = "Александр";
+            // Console.WriteLine($"Меня зовут {MyName}");
+
+            //string Ask = "Как ваше имя ?";
+            //Console.WriteLine(Ask);
+            //string MyName1 = "";
+            //MyName1 = Console.ReadLine();
+            //Console.WriteLine($"Очень хорошо, {MyName1}");
+
+            //Console.BackgroundColor = ConsoleColor.DarkBlue;
+            //Console.Clear();
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine("======================= Список файлов ==========================");
+            //Console.ForegroundColor = ConsoleColor.White;
+            //string[] f = Directory.GetFiles(Environment.CurrentDirectory);
+            //foreach (string f1 in f)
+            //{
+            //    Console.WriteLine(f1);
+            //}
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine("======================= Список файлов ==========================");
+        }
+    }
+}
